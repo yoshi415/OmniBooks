2372625fa3be1a13879fa66f2e9210c514b38486
Suzanne Wang <suzanneswang@gmail.com>
2015-07-16 12:37:38 -0700

    [refactor] add structured db ref

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,22 +1,21 @@
 angular.module('omnibooks.profile', ['firebase'])
 .controller('profileController',['$scope','fireBase', function ($scope,fireBase) {
-
-  $scope.enterBook = function(title,url,author){
-    if(title !== "" && url !== "" && author !== ""){
+  $scope.enterBook = function(title,url,author,subject){
+    if(title !== "" && url !== "" && author !== "" && subject !== ""){
       console.log('enter!');
-      fireBase.enterBook(title,url,author);
+      var myDataRef = new Firebase('https://blazing-inferno-7614.firebaseio.com/orgs/');
+      fireBase.enterBook(title,url,author,subject,myDataRef);
     }
   }
-
 }])
+
 .factory('fireBase', function($firebaseArray){
-  var myDataRef = new Firebase('https://brilliant-heat-9814.firebaseio.com');
-  var enterBook = function(title,url,author){
-    myDataRef.push({title:title,url:url,author:author});
+  // var myDataRef = new Firebase('https://blazing-inferno-7614.firebaseio.com/orgs/' + $scope.org + '/users/' + $scope.user + '/books');
+  var enterBook = function(title,url,author,subject,fb){
+    fb.push({title:title,url:url,author:author,subject:subject});
   }
   return {
     allbooks:$firebaseArray(myDataRef),
     enterBook:enterBook
   }
-
 })

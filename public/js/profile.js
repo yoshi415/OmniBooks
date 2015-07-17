0a40ab958e590b25b2b29ce7b4da421b7e71f972
Suzanne Wang <suzanneswang@gmail.com>
2015-07-16 20:05:21 -0700

    [refactor] add getUserInfo feature

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,53 +1,38 @@
 angular.module('omnibooks.profile', ['firebase'])
-<<<<<<< HEAD
-.controller('profileController',['$scope','fireBase', function ($scope,fireBase) {
-  $scope.enterBook = function(title,url,author,subject){
-    if(title !== "" && url !== "" && author !== "" && subject !== ""){
-      console.log('enter!');
-      var myDataRef = new Firebase('https://blazing-inferno-7614.firebaseio.com/orgs/');
-      fireBase.enterBook(title,url,author,subject,myDataRef);
-    }
-  }
-}])
 
-.factory('fireBase', function($firebaseArray){
-  // var myDataRef = new Firebase('https://blazing-inferno-7614.firebaseio.com/orgs/' + $scope.org + '/users/' + $scope.user + '/books');
-  var enterBook = function(title,url,author,subject,fb){
-    fb.push({title:title,url:url,author:author,subject:subject});
-  }
-  return {
-    allbooks:$firebaseArray(myDataRef),
-    enterBook:enterBook
-  }
-})
-=======
   .controller('profileController', ['$scope', 'fireBase', function($scope, fireBase) {
-
     $scope.enterBook = function(title, url, author) {
-      if (title !== "" && url !== "" && author !== "") {
+      if (title !== "" && url !== "" && author !== "" && subject !== "") {
         console.log('enter!');
-        fireBase.enterBook(title, url, author);
+        fireBase.enterBook(title, url, author, subject);
       }
     };
+    $scope.profile = getUserInfo(id);
   }])
+
   .factory('fireBase', function($firebaseArray, $firebaseObject) {
     var myDataRef = new Firebase('https://brilliant-heat-9814.firebaseio.com');
-    var enterBook = function(title, url, author) {
+    var enterBook = function(title, url, author, subject) {
       var str = randomString();
       myDataRef.push({
         title: title,
         url: url,
         author: author,
+        subject: subject
       });
     };
     var getBook = function(id) {
       var temp = myDataRef.child(id);
       return $firebaseObject(temp);
     };
+    var getUserInfo = function(id) {
+      return $firebaseObject(myDataRef.child(id)); //returns object with user details
+    }
+
     return {
       allbooks: $firebaseArray(myDataRef),
       enterBook: enterBook,
-      getBook: getBook
+      getBook: getBook,
+      getUserInfo: getUserInfo
     };
   });
->>>>>>> be7676acc7ea6a10a763070df7caae09e3009a67

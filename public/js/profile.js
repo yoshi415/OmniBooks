95163c3e2ebb7f6541307a0c2e28c7d3a273ae88
Yoshio Varney <yoshio.varney@gmail.com>
2015-07-22 15:41:33 -0700

    Merge branch 'szwang-master' into dev

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,22 +1,41 @@
 angular.module('omnibooks.profile', ['ui.bootstrap'])
 
-.controller('profileController', ['$scope', 'fireBase', '$stateParams', '$modal', function($scope, fireBase, $stateParams, $modal) {
+.controller('profileController', ['$scope', 'fireBase', '$stateParams', '$modal', '$state', 'auth', 
+  function($scope, fireBase, $stateParams, $modal, $state, auth) {
   $scope.enterBook = function(title, url, author, isbn) {
     if (title && url && author && isbn) {
       $scope.error = false;
-      fireBase.enterBook(title, url, author, isbn);
+      fireBase.enterBook($scope.org, $scope.username, title, url, author, isbn);
       console.log('successfully entered');
     } else {
-      $scope.error = "You must fill out all required fields";
+      $scope.error = "*You must fill out all required fields";
     }
   };
-  $scope.userId = $stateParams.userId;
+
+  $scope.username = auth.getUsername().$id;
+  $scope.org = auth.getOrg();
+
+  $scope.books = fireBase.getUserBookshelf($scope.org, $scope.username);
+
+  // get book id in org node 
+  $scope.getBookId = function(book) {
+    return fireBase.getOrgBookId(book);
+  };
+
+  $scope.findDetail = function(book) {
+    var id = $scope.getBookId(book);
+    console.log(id);
+    $stateParams.itemId = id;
+    $state.go("books",{itemId:id});   
+  };
+
+  // modal methods
   $scope.modalShown = false;
   $scope.toggleModal = function() {
     if(!$scope.error) {
       $scope.modalShown = !$scope.modalShown;
     }
-  };
+  }; 
 }])
 
 .directive('modal', function() {

ea712f756be89b99ab87a221f0e2b50099a91655
Yoshio Varney <yoshio.varney@gmail.com>
2015-07-25 15:04:53 -0700

    [fix] slicing isbn if it includes a dash

--- public/js/profile.js
+++ public/js/profile.js
@@ -8,6 +8,10 @@ angular.module('omnibooks.profile', ['ui.bootstrap'])
     $scope.enterBook = function(title, url, author, isbn, price) {
       if (title && url && author && isbn) {
         $scope.error = false;
+        if (isbn.charAt(3) === '-') {
+          isbn = isbn.slice(0, 3) + isbn.slice(4)
+          console.log(isbn)
+        }
         fireBase.enterBook(currentOrg, currentUser.$id, title, url, author, isbn, price);
         console.log('successfully entered');
       } else {

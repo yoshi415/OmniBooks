0bab49bb43683c8daea3aaca2a3c2ef7121269b1
Yoshio Varney <yoshio.varney@gmail.com>
2015-07-27 10:13:28 -0700

    [bug fix] adding node_modules to .gitgnore

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 var PORT_num = process.env.PORT;
 
+// app.set('port', (process.env.PORT || 8000));
+
 app.use(cors());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -63,5 +65,9 @@ app.post('/sendMail', function(req, res) {
     }
   });
 });
+
+// app.listen(app.get('port'), function() {
+//   console.log('OmniBooks is running on port', app.get('port'));
+// });
 
 app.listen(PORT_num || 8000);
